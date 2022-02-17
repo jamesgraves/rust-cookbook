d9834643c4fb92725f2b9d99dbf65d7c228f02b2
James Graves <james.c.graves.jr@gmail.com>
2022-02-16 20:12:56 -0500

    Factor out the example run function

--- src/algorithms/randomness/src/lib.rs
+++ src/algorithms/randomness/src/lib.rs
@@ -1,23 +1,6 @@
-
 #[cfg(test)]
 mod tests {
-    use std::process::Command;
-
-    fn run_example(example_name: &str) {
-        println!("running example: {example_name}");
-        let output = Command::new("cargo")
-            .arg("run")
-            .arg("--example")
-            .arg(example_name)
-            .output().unwrap_or_else(|e| {
-                panic!("failed to execute process: {}", e)
-        });
-
-        if ! output.status.success() {
-            let s = String::from_utf8_lossy(&output.stderr);
-            panic!("{example_name} failed and stderr was:\n{}", s);
-        }
-    }
+    use cookbook_utils::run_example;
 
     #[test]
     fn basic_usage() {
@@ -54,5 +37,3 @@ mod tests {
         run_example("tuple_distribution");
     }
 }
-
-
---
+++ src/cookbook_utils/src/lib.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+
+#[allow(dead_code)]
+pub fn run_example(example_name: &str) {
+    println!("running example: {example_name}");
+    let output = Command::new("cargo")
+        .arg("run")
+        .arg("--example")
+        .arg(example_name)
+        .output().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+
+    if ! output.status.success() {
+        let s = String::from_utf8_lossy(&output.stderr);
+        panic!("{example_name} failed and stderr was:\n{}", s);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
