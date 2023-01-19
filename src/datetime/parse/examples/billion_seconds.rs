4a0223cd7c185876263ae55bc479cc7940e4379c
James Graves <james.c.graves.jr@gmail.com>
2023-01-18 19:30:06 -0600

    Updated to use Option<> returning functions

--- src/datetime/parse/examples/billion_seconds.rs
+++ src/datetime/parse/examples/billion_seconds.rs
@@ -1,13 +1,17 @@
 use chrono::{NaiveDate, NaiveDateTime};
 
 fn main() {
-    let date_time: NaiveDateTime = NaiveDate::from_ymd(2017, 11, 12).and_hms(17, 33, 44);
-    println!(
-        "Number of seconds between 1970-01-01 00:00:00 and {} is {}.",
-        date_time, date_time.timestamp());
+    if let Some(nov_2017_date) = NaiveDate::from_ymd_opt(2017, 11, 12) {
+        if let Some(five_33) = nov_2017_date.and_hms_opt(17, 33, 44) {
+            println!(
+                "Number of seconds between 1970-01-01 00:00:00 and {} is {}.",
+                five_33, five_33.timestamp());
+        }
+    }
 
-    let date_time_after_a_billion_seconds = NaiveDateTime::from_timestamp(1_000_000_000, 0);
-    println!(
-        "Date after a billion seconds since 1970-01-01 00:00:00 was {}.",
-        date_time_after_a_billion_seconds);
+    if let Some(date_time_after_a_billion_seconds) = NaiveDateTime::from_timestamp_opt(1_000_000_000, 0) {
+        println!(
+            "Date after a billion seconds since 1970-01-01 00:00:00 was {}.",
+            date_time_after_a_billion_seconds);
+    }
 }
