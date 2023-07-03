ef17f601919672b053e9991404e4c598b1c9849b
James Graves <james.c.graves.jr@gmail.com>
2023-07-03 15:21:47 +0000

    ENV variable example succeeds by default
    Create a default config file so that the example will run
    correctly without user intervention.

--- src/os/external/examples/read_env_variable.rs
+++ src/os/external/examples/read_env_variable.rs
@@ -3,13 +3,17 @@ use std::fs;
 use std::io::Error;
 
 fn main() -> Result<(), Error> {
+
+    // Create the contents of the default config file
+    fs::write("/tmp/config-xyz", b"hello there!")?;
+
     // read `config_path` from the environment variable `CONFIG`.
     // If `CONFIG` isn't set, fall back to a default config path.
-    let config_path = env::var("CONFIG")
-        .unwrap_or("/tmp/config".to_string());
+    let config_path = env::var("CONFIG_XYZ")
+        .unwrap_or("/tmp/config-xyz".to_string());
 
     let config: String = fs::read_to_string(config_path)?;
-    println!("Config: {}", config);
+    println!("Config file contents: {}", config);
 
     Ok(())
 }
