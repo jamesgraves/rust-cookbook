2313da83a7673d9912afa1e89598fc2a70e0a454
James Graves <james.c.graves.jr@gmail.com>
2023-07-07 07:02:52 -0500

    Fix unused import warning, add TODO

--- src/net/server/src/lib.rs
+++ src/net/server/src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod tests {
+    /*
     use cookbook_utils::run_example;
 
-    /*
+    // FIXME: Run this in parallel with client program.
     #[test]
     fn listen_unused() {
         run_example("listen_unused");
